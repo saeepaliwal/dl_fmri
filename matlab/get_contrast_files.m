134582cc5028db1e05999fa1656b46c04018f3d1
Saee <saee.paliwal@gmail.com>
2014-03-25 14:22:28 +0100

    Pulling contrast 21 and 23

--- matlab/get_contrast_files.m
+++ matlab/get_contrast_files.m
@@ -10,24 +10,36 @@ pathHdrPostfix = '/spm_glm/sslbin9/fixom_bin9/con_0021.hdr';
 % Pull the ag img contrast files
 homePath = '/cluster/home03/itet/paliwals/Saee/dl_fmri/scans/ag_bp/';
 for i = 1:length(agbpSubjects)
-    fullImgPath = [pathPrefix agbpSubjects{i} pathImgPostfix];
-	fullHdrPath = [pathPrefix agbpSubjects{i} pathHdrPostfix];
-	
-	subjPath = [homePath agbpSubjects{i}];
-	mkdir(subjPath);
-	copyfile(fullImgPath,subjPath);
-	copyfile(fullHdrPath,subjPath);
+    subjPath = [homePath agbpSubjects{i}];
+    mkdir(subjPath);
+    for j = [21 23]
+        
+        pathImgPostfix = ['/spm_glm/sslbin9/fixom_bin9/con_00' sprintf('%d',j) '.img'];
+        pathHdrPostfix = ['/spm_glm/sslbin9/fixom_bin9/con_00' sprintf('%d',j) '.hdr'];
+        
+        fullImgPath = [pathPrefix agbpSubjects{i} pathImgPostfix];
+        fullHdrPath = [pathPrefix agbpSubjects{i} pathHdrPostfix];
+        
+        copyfile(fullImgPath,subjPath);
+        copyfile(fullHdrPath,subjPath);
+    end
 end
 
 % Pull the ant img contrast files
 pathPrefix = '/cluster/scratch_xl/shareholder/klaas/igsandra/ant_bp/MRI/scans/';
 homePath = '/cluster/home03/itet/paliwals/Saee/dl_fmri/scans/ant_bp/';
-for i = 1:length(antbpSubjects)
-	fullImgPath = [pathPrefix antbpSubjects{i} pathImgPostfix];
-	fullHdrPath = [pathPrefix antbpSubjects{i} pathHdrPostfix];
 
-	subjPath = [homePath antbpSubjects{i}];
-	mkdir(subjPath);
-	copyfile(fullImgPath,subjPath);
-	copyfile(fullHdrPath,subjPath);
+for i = 1:length(antbpSubjects)
+    subjPath = [homePath antbpSubjects{i}];
+    mkdir(subjPath);
+    for j = [21 23]
+        pathImgPostfix = ['/spm_glm/sslbin9/fixom_bin9/con_00' sprintf('%d',j) '.img'];
+        pathHdrPostfix = ['/spm_glm/sslbin9/fixom_bin9/con_00' sprintf('%d',j) '.hdr'];
+        
+        fullImgPath = [pathPrefix antbpSubjects{i} pathImgPostfix];
+        fullHdrPath = [pathPrefix antbpSubjects{i} pathHdrPostfix];
+        
+        copyfile(fullImgPath,subjPath);
+        copyfile(fullHdrPath,subjPath);
+    end
 end
