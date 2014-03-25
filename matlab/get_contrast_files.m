42290218feac503285083257789bbe979276b01e
Saee Paliwal <paliwals@brutus2.ethz.ch>
2014-03-25 15:59:20 +0100

    Updated contrast_files.

--- matlab/get_contrast_files.m
+++ matlab/get_contrast_files.m
@@ -1,13 +1,11 @@
 function get_contrast_files
 
-[agbpSubjects antbpSubjects] = sandra_subjects_contrast;
+[agbpSubjects, antbpSubjects] = sandra_subjects_contrast;
 
 pathPrefix = '/cluster/scratch_xl/shareholder/klaas/igsandra/ag_bp/MRI/scans/';
-pathImgPostfix = '/spm_glm/sslbin9/fixom_bin9/con_0021.img';
-pathHdrPostfix = '/spm_glm/sslbin9/fixom_bin9/con_0021.hdr';
 
 % Pull the ag img contrast files
-homePath = '/cluster/home03/itet/paliwals/Saee/dl_fmri/scans/ag_bp/';
+homePath = '/cluster/home03/itet/paliwals/Saee/scans/ag_bp/';
 for i = 1:length(agbpSubjects)
     subjPath = [homePath agbpSubjects{i}];
     %mkdir(subjPath);
@@ -26,7 +24,7 @@ end
 
 % Pull the ant img contrast files
 pathPrefix = '/cluster/scratch_xl/shareholder/klaas/igsandra/ant_bp/MRI/scans/';
-homePath = '/cluster/home03/itet/paliwals/Saee/dl_fmri/scans/ant_bp/';
+homePath = '/cluster/home03/itet/paliwals/Saee/scans/ant_bp/';
 
 for i = 1:length(antbpSubjects)
     subjPath = [homePath antbpSubjects{i}];
